c61420777efbcd2b0b31d87b70fc9fb4c81ca02f
AlexanderVak <mamorunokitsune@gmail.com>
2021-08-27 18:17:00 +0300

    add basic read-only task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
+  const tasks = [
+    {
+      title: 'First task',
+      description: 'Its a description',
+      done: false,
+      dueDate: new Date()
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TodoList</h1>
+      <Task task={tasks[0]}/>
     </div>
   );
 }
