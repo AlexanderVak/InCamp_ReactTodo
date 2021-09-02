3e9c3349d1a41dff0c9a2ae55489d1484b6f1a10
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-02 12:39:30 +0300

    get tasks with useEffect

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SingleTask from './SingleTask';
 import "../styles/Tasks.css"
 import { useParams } from 'react-router-dom';
 
-const Tasks = ({ tasks, setTasks, selectedList }) => {
+
+const Tasks = () => {
 
     const {id} = useParams()
-    console.log(id);
+    console.log(id, typeof +id);
+
+    // useEffect(() => {
+    //     const getTasks = async () => {
+    //         try {
+    //           const response = await api.get(`/lists/${id}/tasks`)
+    //           setTasks(response.data)
+    //         } catch (err) {
+    //           if (err.response) {
+    //             console.log(err.response.data);
+    //             console.log(err.response.status);
+    //             console.log(err.response.headers);
+    //           } else {
+    //             console.log(`Error: ${err.message}`);
+    //           }
+    //         }
+    //       }
+    //       getTasks()
+
+    // }, [selectedList]);
+
+    const { tasks, setTasks } = useFetchTasks(+id)
     return (
         <div className="tasks-container">
             <div className="tasks">
-                {tasks.filter(task => task.listId === selectedList.id).map(task => (
+                {tasks.filter(task => task.listId === +id).map(task => (
                     <SingleTask key={task.id} task={task} setTasks={setTasks} tasks={tasks} />
                 ))}
             </div>
