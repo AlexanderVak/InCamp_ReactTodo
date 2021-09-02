fd7f821dc964e26b167566b86a980f4ecce9b1eb
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-02 16:09:44 +0300

    dsipatch all lists from db

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import List from './List';
 import "../styles/Lists.css"
+import { useDispatch, useSelector } from 'react-redux';
+import { getLists } from '../store/dashboard/actions';
 
 const Lists = ({todoLists, selectedList, setSelectedList}) => {
 
+    const dispatch = useDispatch()
+    useEffect(() => {
+      dispatch(getLists)
+    }, [dispatch]);
+  
+    const lists = useSelector(state => state.dashboard)
+    
     return (
         <ul className="lists-menu">
             <li>
-                {todoLists.map(list => 
+                {lists.map(list => 
                     <List key={list.id} list={list} todoLists={todoLists} selectedList={selectedList} setSelectedList={setSelectedList}/>
                 )}
             </li>
