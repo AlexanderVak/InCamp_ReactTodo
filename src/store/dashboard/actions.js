0cd9b460392dfc4711c27a7fc2ed3200e35f22bb
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-02 16:09:10 +0300

    add actions of dashboard

---
+++ src/store/dashboard/actions.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+export const LOAD_DASHBOARD = "dashboard/load"
+export const SELECTED_LIST = "list/selected"
+
+export const getLists = async (dispatch) => {
+    try {
+      const response = await axios.get('/lists')
+      dispatch({
+          type: LOAD_DASHBOARD,
+          payload: response.data
+      })  
+    
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+}
+
+export const selectList = async (dispatch, id) => {
+    try {
+      const response = await axios.get(`/lists/${id}`)
+      dispatch({
+          type: SELECTED_LIST,
+          payload: response.data
+      })  
+    
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+}
+
