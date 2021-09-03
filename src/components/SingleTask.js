b9ac8c131677b5caf1b8e2ee935101300d67c04e
AlexanderVak <mamorunokitsune@gmail.com>
2021-09-03 12:57:40 +0300

    remove extra code from tasks component

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { changeStatus } from '../store/tasks/actions';
 import '../styles/SingleTask.css'
 
 const SingleTask = ({task}) => {
     const {title, description, done, dueDate} = task
-    
-    // const deleteHandler = () => {
-    //     setTasks(tasks.filter(item => item.id !== task.id))
-    // }
-
-    // const checkboxHandler = () => {
-    //     setTasks(tasks.map(item => {
-    //         if (item.id === task.id) {
-    //             return{
-    //                 ...item, done: !item.done
-    //             }
-    //         }
-    //         return item
-    //     }))
-    // }
-    const dispatch = useDispatch()
 
     const isOverdue = () => new Date(task.dueDate) < new Date()
     const showDate = () => !dueDate ? "" : new Date(dueDate).toLocaleDateString('uk')
+
 
     return (
         <div className="single-task">
             <div className="task-content">
                 <div className="heading">
-                    <label className={`${task.done ? 'task-done' : ''}`}>
-                        <input onChange={changeStatus(task.id, task.listId, task.done)}type="checkbox" name="done" checked={ done }/>
+                    <label className={`${done ? 'task-done' : ''}`}>
+                        <input onChange={changeStatus(task.id, task.listId, done)}type="checkbox" name="done" checked={ done }/>
                         {title}
                     </label>
                 </div>
